d8d17cbc46a7b060b1b9ec43461c453142464a71
Adewale Folorunsho <walefolorunsho45@gmail.com>
2025-03-30 17:33:49 -0700

    Initial Commit

---
+++ CJWRules/CJWRulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CJWRulesApp.swift
+//  CJWRules
+//
+//  Created by Adewale Folorunsho on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CJWRulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
